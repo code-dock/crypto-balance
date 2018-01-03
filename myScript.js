4c87750721adf9b49d33fdf5cb1de1a0b924ae69
Marcelo Lazaroni <lazaronijunior@gmail.com>
2018-01-03 14:31:07 +0000

    list items

--- myScript.js
+++ myScript.js
@@ -1,10 +1,16 @@
 // chrome.runtime.sendMessage("Cryptosaurus");
-fetch("http://localhost:8080/rates")
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(jsonResponse) {
-        const aString = JSON.stringify(jsonResponse);
 
-        document.querySelector("body").textContent = aString;
+function parseAsJson(response) {
+    return response.json();
+}
+
+function addToBody(list) {
+    list.forEach(item => {
+        const info = item.data.base + ": " + item.data.amount;
+        document.body.textContent = document.body.textContent + "<br>" + info;
     });
+}
+
+fetch("http://localhost:8080/rates")
+    .then(parseAsJson)
+    .then(addToBody);
