61fbd0564012080fc75bc57c43a453a8d9a0f3cf
Marcelo Lazaroni <lazaronijunior@gmail.com>
2018-01-03 14:06:06 +0000

    Fetch data

--- myScript.js
+++ myScript.js
@@ -1,1 +1,10 @@
 // chrome.runtime.sendMessage("Cryptosaurus");
+fetch("http://localhost:8080/rates")
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(jsonResponse) {
+        const aString = JSON.stringify(jsonResponse);
+
+        document.querySelector("body").textContent = aString;
+    });
