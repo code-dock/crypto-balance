e01d68ef5cc9fedce515a60a05639271ac92025e
Marcelo Lazaroni <lazaronijunior@gmail.com>
2018-01-15 20:02:00 +0000

    Add table rows

--- myScript.js
+++ myScript.js
@@ -1,66 +1,55 @@
 // chrome.runtime.sendMessage("Cryptosaurus");
 
 function parseAsJson(response) {
-  return response.json();
+    return response.json();
 }
 
 function addRatesToBody(list) {
-  //   list.forEach(item => {
-  //     const myDiv = document.createElement("div");
-  //     myDiv.classList.add("rate-item");
-  //
-  //     const myP = document.createElement("p");
-  //     var className = myP.setAttribute("class", name);
-  //
-  //     myP.textContent =
-  //       item.data.base +
-  //       " 1" +
-  //       " : " +
-  //       item.data.amount +
-  //       " " +
-  //       item.data.currency;
-  //
-  //     myDiv.appendChild(myP);
-  //     document.querySelector(".rates").appendChild(myDiv);
-  //
-  //   });
-  // }
+    list.forEach(item => {
+        const theTable = document.querySelector(".rates table");
 
-  list.forEach(item => {
-    const myTabRow = document.createElement("tr");
-    const myNewCell = document.createElement("td");
-
-    myNewCell.textContent = item.data.base;
-
-    myNewCell.setAttribute("class", "cell-item currency base-currency");
-    myTabRow.appendChild(myNewCell);
-    document.querySelector("table").appendChild(myTabRow);
-  });
+        // Here we are saying that the table's HTML content is
+        // whatever is already there, plus this big string.
+        // This means that we are adding content to the end of the table
+        theTable.innerHTML =
+            theTable.innerHTML +
+            // A backtick like this one here ` means that we are starting a
+            // string that will span through multiple lines.
+            // Inside this strings, we can make reference to variables by
+            // writing them like this ${variableName}
+            `<tr class="table-row">
+                <td class="cell-item currency base-currency">${item.data.base}</td>
+                <td class="cell-item adjust-number add-border">1</td>
+                <td class="cell-item icon"> ➡️ </td>
+                <td class="cell-item fixed-number">${item.data.amount}</td>
+                <td class="cell-item currency native-currency">${item.data.currency}</td>
+            </tr>`;
+    });
 }
 
 function addAccountsToBody(list) {
-  list.forEach(item => {
-    const myDiv = document.createElement("div");
-    myDiv.classList.add("wallet-item");
+    list.forEach(item => {
+        const myDiv = document.createElement("div");
+        myDiv.classList.add("wallet-item");
 
-    const myP = document.createElement("p");
-    myP.textContent =
-      item.balance.amount +
-      " " +
-      item.balance.currency +
-      " : " +
-      item.native_balance.amount +
-      " " +
-      item.native_balance.currency;
-    myDiv.appendChild(myP);
-    document.querySelector(".wallets").appendChild(myDiv);
-  });
+        const myP = document.createElement("p");
+        myP.textContent =
+            item.balance.amount +
+            " " +
+            item.balance.currency +
+            " : " +
+            item.native_balance.amount +
+            " " +
+            item.native_balance.currency;
+        myDiv.appendChild(myP);
+        document.querySelector(".wallets").appendChild(myDiv);
+    });
 }
 
 fetch("http://localhost:8080/rates")
-  .then(parseAsJson)
-  .then(addRatesToBody);
+    .then(parseAsJson)
+    .then(addRatesToBody);
 
 fetch("http://localhost:8080/accounts")
-  .then(parseAsJson)
-  .then(addAccountsToBody);
+    .then(parseAsJson)
+    .then(addAccountsToBody);
