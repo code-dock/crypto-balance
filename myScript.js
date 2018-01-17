aced44cc9bfc956a9c88690f19b442870ae4019f
Jack Murphy <jackmurphy756@gmail.com>
2018-01-17 21:14:58 +0000

    changed icons

--- myScript.js
+++ myScript.js
@@ -1,139 +1,146 @@
 /* eslint-disable */
 
 function parseAsJson(response) {
-    return response.json();
+  return response.json();
 }
 
 function addRatesToBody(list) {
-    list.forEach(item => {
-        const ratesTable = document.querySelector(".rates table .table-row");
+  list.forEach(item => {
+    const ratesTable = document.querySelector(".rates table .table-row");
 
-        // Here we are saying that the table's HTML content is
-        // whatever is already there, plus this big string.
-        // This means that we are adding content to the end of the table
-        ratesTable.innerHTML =
-            ratesTable.innerHTML +
-            // A backtick like this one here ` means that we are starting a
-            // string that will span through multiple lines.
-            // Inside this strings, we can make reference to variables by
-            // writing them like this ${variableName}
-            `
+    // Here we are saying that the table's HTML content is
+    // whatever is already there, plus this big string.
+    // This means that we are adding content to the end of the table
+    ratesTable.innerHTML =
+      ratesTable.innerHTML +
+      // A backtick like this one here ` means that we are starting a
+      // string that will span through multiple lines.
+      // Inside this strings, we can make reference to variables by
+      // writing them like this ${variableName}
+      `
                 <td class="cell-item">${item.data.base}•</td>
                 <td class="cell-item">£${item.data.amount}</td>
             `;
-    });
+  });
 }
 
 function getSymbol(abr) {
-    if (abr === "GBP") {
-        return "£";
-    }
+  if (abr === "GBP") {
+    return "£";
+  }
 
-    if (abr === "USD") {
-        return "$";
-    }
+  if (abr === "USD") {
+    return "$";
+  }
 
-    if (abr === "EUR") {
-        return "€";
-    }
-    return abr;
+  if (abr === "EUR") {
+    return "€";
+  }
+  return abr;
 }
 
 function getIcon(abr) {
-    if (abr === "BCH") {
-        return "bch.png";
-    }
+  if (abr === "BCH") {
+    return "bch.png";
+  }
 
-    if (abr === "LTC") {
-        return "ltc.png";
-    }
+  if (abr === "LTC") {
+    return "ltc.png";
+  }
 
-    if (abr === "ETH") {
-        return "eth.jpg";
-    }
+  if (abr === "ETH") {
+    return "eth.jpg";
+  }
 
-    if (abr === "BTC") {
-        return "btc.png";
-    }
+  if (abr === "BTC") {
+    return "btc.png";
+  }
 
-    return "placeholder.jpg";
+  if (abr === "EUR") {
+    return "eur.png";
+  }
+  if (abr === "USD") {
+    return "usd.png";
+  }
+
+  return "placeholder.png";
 }
 
 function getExtendedName(abr) {
-    if (abr === "BCH") {
-        return "Bitcoin Cash";
-    }
+  if (abr === "BCH") {
+    return "Bitcoin Cash";
+  }
 
-    if (abr === "LTC") {
-        return "Litecoin";
-    }
+  if (abr === "LTC") {
+    return "Litecoin";
+  }
 
-    if (abr === "ETH") {
-        return "Etherium";
-    }
+  if (abr === "ETH") {
+    return "Etherium";
+  }
 
-    if (abr === "BTC") {
-        return "Bitcoin";
-    }
+  if (abr === "BTC") {
+    return "Bitcoin";
+  }
 
-    if (abr === "GBP") {
-        return "British Pound";
-    }
+  if (abr === "GBP") {
+    return "British Pound";
+  }
 
-    if (abr === "EUR") {
-        return "Euro";
-    }
+  if (abr === "EUR") {
+    return "Euro";
+  }
 
-    if (abr === "USD") {
-        return "US Dollar";
-    }
+  if (abr === "USD") {
+    return "US Dollar";
+  }
 
-    return abr;
+  return abr;
 }
 
 function addAccountsToBody(list) {
-    const walletsTable = document.querySelector(".wallets table");
+  const walletsTable = document.querySelector(".wallets table");
 
-    list.forEach((item, index) => {
-        const icon =
-            '<td class="cell-item icon">' +
-            '<img class="crypto-img" src="' +
-            getIcon(item.balance.currency) +
-            '" alt="">' +
-            "</td>";
+  list.forEach((item, index) => {
+    const icon =
+      '<td class="cell-item icon">' +
+      '<img class="crypto-img" src="' +
+      getIcon(item.balance.currency) +
+      '" alt="">' +
+      "</td>";
 
-        const currencyName =
-            '<td class="cell-item base-currency name-currency">' +
-            getExtendedName(item.balance.currency) +
-            "</td>";
+    const currencyName =
+      '<td class="cell-item base-currency name-currency">' +
+      getExtendedName(item.balance.currency) +
+      "</td>";
 
-        const balance =
-            '<td class="cell-item balance">' +
-            '<text class="base-currency">' +
-            getExtendedName(item.balance.currency) +
-            "</text>" +
-            "<br>" +
-            '<text class="native-currency">' +
-            getSymbol(item.native_balance.currency) +
-            " " +
-            item.native_balance.amount +
-            "</text>" +
-            "</td>";
+    const balance =
+      '<td class="cell-item balance">' +
+      '<text class="base-currency">' +
+      getExtendedName(item.balance.currency) +
+      "</text>" +
+      "<br>" +
+      '<text class="native-currency">' +
+      getSymbol(item.native_balance.currency) +
+      " " +
+      item.native_balance.amount +
+      "</text>" +
+      "</td>";
 
-        walletsTable.innerHTML =
-            walletsTable.innerHTML +
-            '<tr class="table-row">' +
-            icon +
-            currencyName +
-            balance +
-            "</tr>";
-    });
+    walletsTable.innerHTML =
+      walletsTable.innerHTML +
+      '<tr class="table-row">' +
+      icon +
+      currencyName +
+      balance +
+      "</tr>";
+  });
 }
 
 fetch("http://localhost:8080/rates")
-    .then(parseAsJson)
-    .then(addRatesToBody);
+  .then(parseAsJson)
+  .then(addRatesToBody);
 
 fetch("http://localhost:8080/accounts")
-    .then(parseAsJson)
-    .then(addAccountsToBody);
+  .then(parseAsJson)
+  .then(addAccountsToBody);
