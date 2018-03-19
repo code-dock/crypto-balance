dcc51cc31c813ce36601c0b26b6b3c49a72e0de9
Marcelo Lazaroni <lazaronijunior@gmail.com>
2018-03-19 20:20:24 +0000

    Complete OAuth flow using temporary userID

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@
 const Client = require("coinbase").Client;
 const credentials = require("root-require")("./server/.credentials.json");
 const purifier = require("root-require")("./server/lib/routePurifier");
-
+const errors = require("root-require")("./server/lib/errors");
 const express = require("express");
 const Future = require("fluture");
 const { traverse } = require("ramda");
@@ -21,13 +21,13 @@ const DOMAIN = `http://localhost:${PORT}`;
 const app = express();
 
 const postRequest = (url, options) =>
-    Future((reject, resolve) =>
+    Future((reject, resolve) => {
         request.post(
             url,
             options,
             (err, res) => (err ? reject(err) : resolve(res))
-        )
-    );
+        );
+    });
 
 // const client = new Client({
 //     apiKey: credentials.apiKey,
@@ -77,14 +77,36 @@ const postRequest = (url, options) =>
 // OAuth
 // ============================================================================
 const redirectUri = DOMAIN + "/oauth/callback";
+const database = {};
 
-app.use(express.static("./server/static"));
+const isLoggedIn = (req, res, next) => {
+    const userID = req.ip;
+    // const userID = req.get("X-USER-ID");
+    const userInfo = database[userID];
+
+    if (!userInfo) {
+        const err = errors.unauthorised("User not logged in.");
+        const pureResponse = purifier.respond.custom(err);
+        return purifier.route(pureResponse)(req, res, next);
+    }
+
+    const { accessToken, refreshToken } = userInfo;
+    req.coinbaseClient = new Client({ accessToken, refreshToken });
+    return next();
+};
+
+app.use(express.static(`${__dirname}/static`));
+
+app.get("/test", isLoggedIn, (req, res) => res.send("Works!"));
 
 app.get(
     "/oauth/callback",
     purifier.route(req => {
         const code = req.query.code;
+        const userID = req.ip;
+
         console.log("Code", code);
+        console.log("UserID", userID);
 
         return postRequest("https://api.coinbase.com/oauth/token", {
             json: {
@@ -95,8 +117,14 @@ app.get(
                 redirect_uri: redirectUri
             }
         })
-            .map(res => console.log(res.body))
-            .map(_ => purifier.redirect({ path: "/success" }));
+            .map(res => {
+                database[userID] = {
+                    accessToken: res.body.access_token,
+                    refresh_token: res.body.refresh_token
+                };
+                console.log(database[userID]);
+            })
+            .map(_ => purifier.respond.redirect({ path: "/success.html" }));
     })
 );
 
